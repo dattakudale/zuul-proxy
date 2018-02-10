ecce1df4b9a424e4677deb4a15e13240aa27e82f
33438062+dkudale@users.noreply.github.com <33438062+dkudale@users.noreply.github.com>
2018-02-10 14:48:03 -0500

    Added zuul proxy

---
+++ zuul-proxy/src/main/java/com/dk/eurekaserver/ZuulProxyApplication.java
@@ -0,0 +1,24 @@
+package com.dk.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.dk.eurekaserver.filter.CustomZuulFilter;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZuulProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulProxyApplication.class, args);
+	}
+	
+	@Bean
+	public CustomZuulFilter addResponseHeaderFilter() {
+		return new CustomZuulFilter();
+	}
+}
---
+++ zuul-proxy/src/main/java/com/dk/eurekaserver/filter/CustomZuulFilter.java
@@ -0,0 +1,51 @@
+package com.dk.eurekaserver.filter;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class CustomZuulFilter extends ZuulFilter {
+
+	  private static Logger log = LoggerFactory.getLogger(CustomZuulFilter.class);
+	
+	@Override
+	public Object run() {
+		RequestContext context = RequestContext.getCurrentContext();
+		
+		HttpServletRequest request = context.getRequest();
+		
+		String token = request.getHeader("JWT");
+		
+		log.info("JWT : "+token);
+		
+		HttpServletResponse servletResponse = context.getResponse();
+		servletResponse.addHeader("CustomHeader", UUID.randomUUID().toString());
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+
+		return 999;
+	}
+
+	@Override
+	public String filterType() {
+
+		return "pre";
+	}
+
+}
